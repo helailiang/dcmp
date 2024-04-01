b97442a5ef60e905252b91207e204cd678cce931
helailiang <helailiang@reddatetech.com>
2024-04-01 14:20:04 +0800

    init: create

---
+++ dcmp-gateway/README.md
@@ -0,0 +1,19 @@
+# dcmp use fabric example
+
+### 1. 取dcmp 门户的Access Management页面选择接入key 导出
+
+![image-20240401135406212](README.assets/image-20240401135406212.png)
+
+### 2. 解压下载得到的zip文件
+
+可以得到`接入节点地址`和`接入证书信息`
+
+接入节点地址： 可以在下面文件中的excel文件中获取,  获取文件中 grpc 地址： grpcs://192.168.1.66:32444
+
+接入证书信息： 可以在peerkess.zip中获取，里面包含msp和tls两个目录
+
+### 3. 使用fabric-gateway进行接入
+
+可以参考`fabric-gateway`官方说明。<a href="https://hyperledger-fabric.readthedocs.io/en/latest/gateway.html" target="_blank">了解更多</a>
+
+具体例子可以参考：
---
+++ dcmp-gateway/assetTransfer.go
@@ -0,0 +1,180 @@
+/*
+Copyright 2021 IBM All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package main
+
+import (
+	"bytes"
+	"crypto/x509"
+	"encoding/json"
+	"fmt"
+	"os"
+	"path"
+	"time"
+
+	"github.com/hyperledger/fabric-gateway/pkg/client"
+	"github.com/hyperledger/fabric-gateway/pkg/identity"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+)
+
+const (
+	mspID        = "Myorg1MSP"
+	certPath     = "certs/7c663c1a986a137dde3ac59e1fad8132b4cb7854/msp/signcerts/cert.pem"
+	keyPath      = "certs/7c663c1a986a137dde3ac59e1fad8132b4cb7854/msp/keystore"
+	tlsCertPath  = "certs/7c663c1a986a137dde3ac59e1fad8132b4cb7854/tls/tlsintermediatecerts/tls-ca-myorg1--0.pem"
+	peerEndpoint = "192.168.1.66:32444"
+	gatewayPeer  = "peer1.myorg1-no2024032800012.athens.bsnbase.com"
+)
+
+var now = time.Now()
+var assetId = fmt.Sprintf("asset%d", now.Unix()*1e3+int64(now.Nanosecond())/1e6)
+
+func main() {
+	fmt.Println(os.Getwd())
+	// The gRPC client connection should be shared by all Gateway connections to this endpoint
+	clientConnection := newGrpcConnection()
+	defer clientConnection.Close()
+
+	id := newIdentity()
+	sign := newSign()
+
+	// Create a Gateway connection for a specific client identity
+	gw, err := client.Connect(
+		id,
+		client.WithSign(sign),
+		client.WithClientConnection(clientConnection),
+		// Default timeouts for different gRPC calls
+		client.WithEvaluateTimeout(1005*time.Second),
+		client.WithEndorseTimeout(1005*time.Second),
+		client.WithSubmitTimeout(1005*time.Second),
+		client.WithCommitStatusTimeout(100*time.Minute),
+	)
+	if err != nil {
+		panic(err)
+	}
+	defer gw.Close()
+
+	// Override default values for chaincode and channel name as they may differ in testing contexts.
+	chaincodeName := "dcmpchanincode"
+	if ccname := os.Getenv("CHAINCODE_NAME"); ccname != "" {
+		chaincodeName = ccname
+	}
+
+	channelName := "mychannel01"
+	if cname := os.Getenv("CHANNEL_NAME"); cname != "" {
+		channelName = cname
+	}
+	network := gw.GetNetwork(channelName)
+	contract := network.GetContract(chaincodeName)
+
+	createAsset(contract)
+	getAllAssets(contract)
+
+}
+
+// newGrpcConnection creates a gRPC connection to the Gateway server.
+func newGrpcConnection() *grpc.ClientConn {
+	// TLS 证书
+	certificate, err := loadCertificate(tlsCertPath)
+	if err != nil {
+		panic(err)
+	}
+
+	certPool := x509.NewCertPool()
+	certPool.AddCert(certificate)
+	transportCredentials := credentials.NewClientTLSFromCert(certPool, gatewayPeer)
+
+	connection, err := grpc.Dial(peerEndpoint, grpc.WithTransportCredentials(transportCredentials))
+	if err != nil {
+		panic(fmt.Errorf("failed to create gRPC connection: %w", err))
+	}
+
+	return connection
+}
+
+// newIdentity creates a client identity for this Gateway connection using an X.509 certificate.
+// 交易的客户端标识
+func newIdentity() *identity.X509Identity {
+	certificate, err := loadCertificate(certPath)
+	if err != nil {
+		panic(err)
+	}
+
+	id, err := identity.NewX509Identity(mspID, certificate)
+	if err != nil {
+		panic(err)
+	}
+
+	return id
+}
+
+func loadCertificate(filename string) (*x509.Certificate, error) {
+	certificatePEM, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read certificate file: %w", err)
+	}
+	return identity.CertificateFromPEM(certificatePEM)
+}
+
+// newSign creates a function that generates a digital signature from a message digest using a private key.
+func newSign() identity.Sign {
+	files, err := os.ReadDir(keyPath)
+	if err != nil {
+		panic(fmt.Errorf("failed to read private key directory: %w", err))
+	}
+	privateKeyPEM, err := os.ReadFile(path.Join(keyPath, files[0].Name()))
+
+	if err != nil {
+		panic(fmt.Errorf("failed to read private key file: %w", err))
+	}
+
+	privateKey, err := identity.PrivateKeyFromPEM(privateKeyPEM)
+	if err != nil {
+		panic(err)
+	}
+
+	sign, err := identity.NewPrivateKeySign(privateKey)
+	if err != nil {
+		panic(err)
+	}
+
+	return sign
+}
+
+// Evaluate a transaction to query ledger state.
+func getAllAssets(contract *client.Contract) {
+	fmt.Println("\n--> Evaluate Transaction: GetAllAssets, function returns all the current assets on the ledger")
+
+	evaluateResult, err := contract.EvaluateTransaction("Get", "Tom")
+	if err != nil {
+		panic(fmt.Errorf("failed to evaluate transaction: %w", err))
+	}
+	result := formatJSON(evaluateResult)
+
+	fmt.Printf("*** Result:%s\n", result)
+}
+
+// Submit a transaction synchronously, blocking until it has been committed to the ledger.
+func createAsset(contract *client.Contract) {
+	fmt.Printf("\n--> 调用set() \n")
+
+	_, err := contract.SubmitTransaction("set", "Tom", "38800")
+	if err != nil {
+		panic(fmt.Errorf("failed to submit transaction: %w", err))
+	}
+
+	fmt.Printf("*** Transaction committed successfully\n")
+}
+
+// Format JSON data
+func formatJSON(data []byte) string {
+	var prettyJSON bytes.Buffer
+	if err := json.Indent(&prettyJSON, data, "", "  "); err != nil {
+		panic(fmt.Errorf("failed to parse JSON: %w", err))
+	}
+	return prettyJSON.String()
+}
